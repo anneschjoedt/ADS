bc34964e2dae33f9ee03551f8e93c0f8e7e25cb3
Riko Jacob (@ITU Air 13) <rikj@itu.dk>
2018-02-15 08:04:19 +0100

    plant a random quadruple

--- foursum/src/Triple.py
+++ foursum/src/Triple.py
@@ -15,11 +15,20 @@ def _main():
     #print(L)
     print('\n'.join(str(i) for i in L))
 
-def triples(N,M, seed=None):
+def triples(N,M, seed=None, shuffled=True,result=False):
     if seed != None:
         random.seed(seed)
     L=list(range(int(-M/2),int(-M/2+1))) + list( range(1,N)) + list(range(M-N,M-1))
-    random.shuffle(L)
+    if result:
+        i=random.randint(3,len(L)-1)
+        j=random.randint(2,i-1)
+        k=random.randint(1,j-1)
+        l=random.randint(0,k-1)
+        print(i,j,k,l)
+        L[j] = - (L[i]+L[k]+L[l])
+        
+    if shuffled:
+        random.shuffle(L)
     return L
 
 
