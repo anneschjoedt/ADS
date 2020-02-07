65228ff45bcc597540e0d233613f672456bebe7b
Holger Dell <holgerdell@users.noreply.github.com>
2020-02-07 12:56:04 +0100

    Simple.java: Tabs vs. Spaces; curly braces

--- foursum/sol/Simple.java
+++ foursum/sol/Simple.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Simple
 {
-    public static void main(String[] args)
-    {
-	Scanner S= new Scanner(System.in);
-	int N = Integer.parseInt(S.nextLine());
-	long[] vals = new long[N];
-	for(int i= 0; i < N; i+= 1) vals[i] = Long.parseLong(S.nextLine());
-
-	for (int i = 0; i < N; i+= 1) // i goes through {0, ..., N-1}
-              // your code goes here
-
-
+  public static void main(String[] args)
+  {
+    Scanner S = new Scanner(System.in);
+    int N = Integer.parseInt(S.nextLine());
+    long[] vals = new long[N];
+    for(int i = 0; i < N; i += 1) {
+      vals[i] = Long.parseLong(S.nextLine());
     }
+
+    for (int i = 0; i < N; i += 1) { // i goes through {0,...,N-1}
+      // your code goes here
+    }
+  }
 }
